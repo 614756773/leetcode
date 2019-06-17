aac1a471707059789c5e4ed257dd6ce4ddc79e66
qinzhu-lhq <17721875524@163.com>
2019-06-17 10:21:50 +0800

    加强版hash排序

--- src/other/加强版的hash排序.java
+++ src/other/加强版的hash排序.java
@@ -41,24 +41,49 @@ public class 加强版的hash排序 {
     };
 
     public static void main(String[] args) {
-        int[] nums = new int[]{16, 1, 32, 26, 3, 19, 20, 5, 7, 9, 10};
-        sort_(nums);
+        int[] nums = new int[]{1, 3, 4, 132, 5, 7, 456, 9, 10, 12, 13, 78, 16, 465, 19, 20, 213, 23, 89, 26, 94, 32, 97, 47, 48, 56, 123};
+//        sort_(nums);
         sort(nums);
     }
 
+    /**
+     *
+     * @param nums 要求数字没有重复，排序0~1亿的数需要11MB左右的内存
+     */
     private static void sort(int[] nums) {
+        // 1.确定hash桶的数量，一个hash桶最多可以存放32个数
         int max = max(nums);
-        // 等价于(max / 32) + max % 32;
-        int n = max >> 5 + max & (31);
-        // n个hash桶，一个hash桶最多可以存放32个数
+        int n = (max >> 5) + (max & (31));// 等价于(max / 32) + max % 32;
         int buckets[] = new int[n];
-        // TODO 写到这儿了
         for (int num : nums) {
-            int bucketsIndex = num >> 5 - 1;
-            if ((num & 31) == 0 ) {
-                bucketsIndex--;
+            // 2.确定桶索引和桶内索引
+            int bucketsIndex = (num >> 5) - 1;
+            int bucketIndex;
+            int tmp = num & 31;
+            if ((tmp) != 0) {
+                bucketsIndex++;
+                bucketIndex = tmp - 1;
+            } else {
+                if (num == 0) {
+                    throw new RuntimeException("暂时不支持含0的排序，因为1~32存放在第一个int中，33~64存放在第二个int中...");
+                } else {
+                    bucketIndex = 31;
+                }
             }
-            int bucketIndex = num & 31;
+            // 3.用为操作将该数记录
+            buckets[bucketsIndex] = buckets[bucketsIndex] | masks[bucketIndex];
+        }
+        // 4.输出
+        for (int i = 0; i < buckets.length; i++) {
+            int bucket = buckets[i];
+            for (int j = 0; j < 32; j++) {
+                int tmp = bucket & masks[j];
+                tmp = tmp >> (32 - 1 - j);
+                if (tmp == 1 || tmp == -1) {
+                    int result = i * 32 + (j + 1);
+                    System.out.print(result + ",");
+                }
+            }
         }
     }
 
@@ -66,6 +91,10 @@ public class 加强版的hash排序 {
      * @param nums 要求最大数小于等于32，并且没有重复
      */
     private static void sort_(int[] nums) {
+        if (max(nums) > 32) {
+            throw new RuntimeException("都说了这个低配DEMO不支持含32以上的数");
+        }
+        
         // 用1个int作hash桶，可以存放32个数字
         int bucket = 0b00000000000000000000000000000000;
         for (int num : nums) {
@@ -74,7 +103,9 @@ public class 加强版的hash排序 {
         for (int i = 0; i < 32; i++) {
             int bitValue = bucket & masks[i];
             bitValue = bitValue >> (32 - i - 1);
-            if (bitValue == 1) {
+            // 等于1时表明这个位之前被标记过，不过有一个特殊的情况：
+            // 32位中最左边那一位是符号位，用位运算取出来的该位值虽然是1但是结果会被变成-1
+            if (bitValue == 1 || bitValue == -1) {
                 System.out.println(i + 1);
             }
         }
