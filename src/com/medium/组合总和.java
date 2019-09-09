a0b3d54b51260bdc6da520094203785a8be9752c
qinzhu-lhq <17721875524@163.com>
2019-09-09 18:06:36 +0800

    组合总和---待完成

--- src/com/medium/组合总和.java
+++ src/com/medium/组合总和.java
@@ -1,8 +1,12 @@
 package com.medium;
 
+import dataStructure.ListNode;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 /**
  * @Date: 2019/9/3
@@ -16,7 +20,7 @@ public class 组合总和 {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(candidates);
-        backtrack(result, candidates, target, 0, new ArrayList<Integer>());
+        backtrack(result, candidates, target, 0, new LinkedList<>());
         return result;
     }
 
@@ -27,7 +31,30 @@ public class 组合总和 {
                   [2,3,3],
                   [3,5]
                 ]*/
-    private void backtrack(List<List<Integer>> result, int[] candidates, int target, int current, ArrayList<Integer> group) {
+    private void backtrack(List<List<Integer>> result, int[] candidates, int target, int currentIndex, LinkedList<Integer> group) {
+        int currentNumber = candidates[currentIndex];
+        int tmpResult = reduce(group) + currentNumber;
+        if (tmpResult == target) {
+            group.add(currentNumber);
+            result.add(new ArrayList<>(group));
+            // TODO...
+            return;
+        }
 
+        if (tmpResult < target) {
+            group.add(currentNumber);
+            if (reduce(group) + candidates[currentIndex] <= target) {
+                backtrack(result, candidates, target, currentIndex, group);
+            }
+            return;
+        }
+
+        if (tmpResult > target) {
+            group.removeLast();
+        }
+    }
+
+    private int reduce(List<Integer> group) {
+        return group.stream().reduce((a, b) -> a + b).orElse(0);
     }
 }
