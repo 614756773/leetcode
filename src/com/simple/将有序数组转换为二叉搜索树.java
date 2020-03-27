7ebde8e478a8d53b01634c46279df5a4641511f0
614756773@qq.com <1138732041@qq.com>
2020-03-27 23:12:21 +0800

    :sparkles: 将有序数组转换为二叉搜索树

--- src/com/simple/将有序数组转换为二叉搜索树.java
+++ src/com/simple/将有序数组转换为二叉搜索树.java
@@ -5,10 +5,13 @@ import dataStructure.TreeNode;
 /**
  * @author qinzhu
  * @since 2020/3/27
+ * 特点：二叉搜索树的中序遍历是有序的
+ * 思路：根据二叉搜索树的特点优先构造中间节点，然后小的为左子树（递归构造下去），大的为右子树（递归构造下去）
  */
 public class 将有序数组转换为二叉搜索树 {
     public static void main(String[] args) {
-
+        TreeNode root = new 将有序数组转换为二叉搜索树().sortedArrayToBST(new int[]{-10, -3, 0, 5, 9});
+        System.out.println(root);
     }
 
     public TreeNode sortedArrayToBST(int[] nums) {
@@ -18,12 +21,23 @@ public class 将有序数组转换为二叉搜索树 {
         if (nums.length == 1) {
             return new TreeNode(nums[0]);
         }
-        
+
         return dfsProduce(nums, 0, nums.length - 1);
     }
 
     private TreeNode dfsProduce(int[] nums, int left, int right) {
-        int mid; // TODO
-        return null;
+        if (left == right) {
+            return new TreeNode(nums[left]);
+        }
+        if (left > right) {
+            return null;
+        }
+        // mid相对于left的偏移量
+        int move = (right - left) % 2 == 0 ? (right - left) / 2 : (right - left + 1) / 2;
+        int mid = move + left;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfsProduce(nums, left, mid - 1);
+        root.right = dfsProduce(nums, mid + 1, right);
+        return root;
     }
 }
