4e6491c818a339fa2aeb339ee4b1a9dfae328bae
614756773 <614756773@qq.com>
2020-04-03 18:02:17 +0800

    :sparkles: :sparkles: 字符串转换整数（修改）

--- src/com/medium/字符串转换整数.java
+++ src/com/medium/字符串转换整数.java
@@ -1,58 +1,70 @@
 package com.medium;
 
 /**
- * @Date: 2019/6/18
- * @Author: qinzhu
+ * @since 2020-4-3 18:00:39
+ * @author qinzhu
  */
 public class 字符串转换整数 {
     public static void main(String[] args) {
         String[] s = new String[]{"   001", "a0bc123", " a0basd123", " -123ab0123",
-                " 0123asd123", "", "  ","abc",
+                " 0123asd123", "", "  ", "abc",
                 "61475677395", "-2147483648", "2147483647", "-",
                 "+", "-6147483648", "2147483646", "-2147483647"};
+        字符串转换整数 o = new 字符串转换整数();
+        System.out.println(o.myAtoi("42"));
         for (String s1 : s) {
             System.out.println(s1);
-            System.out.println(myAtoi(s1));
+            System.out.println(o.myAtoi(s1));
             System.out.println("_________________");
         }
-//        System.out.println(myAtoi("-6147483648"));
     }
 
     // [48-57]
-    private static int myAtoi(String str) {
-        str = str.trim();
-        if (str.equals("")) {
-            return 0;
-        }
-        boolean isNegative = false;
-        // 负数
-        if (str.charAt(0) == '-') {
-            isNegative = true;
-            str = str.substring(1);
-        } else if(str.charAt(0) == '+') {
-            str = str.substring(1);
-        }
-        if (str.equals("")) {
+    private int myAtoi(String str) {
+        if (str == null || str.length() == 0) {
             return 0;
         }
 
+        char[] chars = str.toCharArray();
         int index = 0;
-        int result = 0;
-        while (index < str.length()) {
-            char c = str.charAt(index++);
-            if (c >= 48 && c <= 57) {
-                if (result > Integer.MAX_VALUE / 10) {
-                    return isNegative ? -(1 << 31) : (1 << 31) -1;
-                }
-                if ((result * 10) > Integer.MAX_VALUE - (c - 48)) {
-                    return isNegative ? -(1 << 31) : (1 << 31) -1;
-                }
-                result = result * 10 + (c - 48);
+        while (index < chars.length) {
+            if (chars[index] == ' ') {
+                index++;
+            } else {
+                break;
             }
-            else {
+        }
+        if (index == chars.length) {
+            return 0;
+        }
+
+        // 负数
+        boolean isNegative = false;
+        if (chars[index] == '-') {
+            isNegative = true;
+            index++;
+        } else if (chars[index] == '+') {
+            isNegative = false;
+            index++;
+        }
+
+        int result = 0;
+        while (index < chars.length) {
+            char c = chars[index++];
+            int num = c - 48;
+            if (isNumber(c)) {
+                if (result > (Integer.MAX_VALUE - num) / 10) {
+                    return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                }
+                result = result * 10 + num;
+            } else {
                 break;
             }
         }
         return isNegative ? -result : result;
+    }
+
+    private boolean isNumber(char c) {
+        return c >= 48 && c <= 57;
     }
 }
