5d28d06fe47ef41d5d1671ba1821dca5054d8af4
hotpot <614756773@qq.com>
2020-09-16 18:01:22 +0800

    :sparkles: 递增顺序查找树_897

--- src/com/simple/递增顺序查找树_897.java
+++ src/com/simple/递增顺序查找树_897.java
@@ -9,8 +9,27 @@ import java.util.List;
  * @author qinzhu
  * @since 2020/9/16
  * 思路：递归中序遍历存入list，然后取出来构造结果
+ * 思路2：使用哑结点，并且不使用额外的空间
  */
 public class 递增顺序查找树_897 {
+
+    private TreeNode dump = new TreeNode(-1);
+    private TreeNode pre = dump;
+
+    public TreeNode increasingBST_2(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        increasingBST_2(root.left);
+
+        pre.right = root;
+        root.left = null;
+        pre = root;
+
+        increasingBST_2(root.right);
+        return dump.right;
+    }
+
     public TreeNode increasingBST(TreeNode root) {
         if (root == null) {
             return null;
