3d5d5c4350d04926657e4281a4ae570aa4590c77
hotpot <17721875524@163.com>
2020-09-27 23:40:59 +0800

    style

--- src/com/simple/二叉搜索树的最近公共祖先.java
+++ src/com/simple/二叉搜索树的最近公共祖先.java
@@ -22,4 +22,21 @@ public class 二叉搜索树的最近公共祖先 {
             return lowestCommonAncestor(root.right, p, q);
         }
     }
+
+    /**
+     * 写法2
+     */
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
 }
