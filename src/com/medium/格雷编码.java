1b8d65c19d96aff36a6f80f3544c957f6b9b80c3
hotpot <614756773@qq.com>
2020-10-13 18:17:36 +0800

    :sparkles: :sparkles: 格雷编码

--- src/com/medium/格雷编码.java
+++ src/com/medium/格雷编码.java
@@ -1,81 +1,84 @@
 package com.medium;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 /**
  * @author qinzhu
  * @since 2020/10/9
+ * 思路：
+ *      方法一，使用递归
+ *          可以预先得知格雷码的数量是2的n次方，比如n为3，则一共有8个格雷码。在递归时使用此数量来结束递归
+ *          使用masks数组做掩码，对当前的格雷码挨个进行异或来求得下一个格雷码（如果在结果集中已经包含了则跳过）
+ *      方法二，规律/镜像反射法
+ *          看图https://leetcode-cn.com/problems/gray-code/solution/gray-code-jing-xiang-fan-she-fa-by-jyd
  */
 public class 格雷编码 {
+    private int[] masks;
+    private int grayCodeNum;
+    private LinkedHashSet<Integer> res = new LinkedHashSet<>();
     public static void main(String[] args) {
-        System.out.println(new 格雷编码().grayCode(2));
-        // TODO 结果有误
-    }
-
-    List<List<Integer>> res = new ArrayList<>();
-    public List<Integer> grayCode(int n) {
-        backtrack(0, n, new ArrayList<>());
-        return convert(res);
-    }
-
-    /**
-     * 将000,001,010...101之类的集合转换为0,1,2...5
-     */
-    private List<Integer> convert(List<List<Integer>> list) {
-        List<Integer> result = new ArrayList<>();
-
-        for (List<Integer> l : list) {
-            StringBuilder sb = new StringBuilder();
-            for (Integer integer : l) {
-                sb.append(integer);
-            }
-            result.add(Integer.valueOf(sb.toString(), 2));
+        List<Integer> x = new 格雷编码().grayCode(0);
+        for (Integer integer : x) {
+            System.out.println(Integer.toString(integer, 2));
         }
-        return result;
+        System.out.println(x);
     }
 
-    /**
-     *
-     * @param level 回溯的层级
-     * @param n     结束条件
-     * @param list  用于存储满足条件的数字及其序列，后续会转换成格雷码
-     */
-    private void backtrack(int level, int n, List<Integer> list) {
-        if (level == n) {
-            if (res.isEmpty()) {
-                res.add(new ArrayList<>(list));
-                return;
-            }
+    public List<Integer> grayCode2(int n) {
+        if (n < 1) {
+            return new ArrayList<>();
+        }
 
-            // 比较与上一个格雷码是否只有1位不同
-            List<Integer> preGrayCode = res.get(res.size() - 1);
-            if (onlyOneBitDifferent(preGrayCode, list)) {
-                res.add(new ArrayList<>(list));
+        // gn为正向的数据
+        List<Integer> gn = new ArrayList<>();
+        // rn为镜像数据
+        List<Integer> rn = new ArrayList<>();
+        gn.add(0);
+        rn.add(1);
+        for (int i = 2; i < n; i++) {
+            // 更新gn
+            for (int j = 0; j < i; j++) {
+                gn.addAll(rn);
             }
+            // 更新rn
+            for (int j = i - 1; j >= 0; j--) {
+                // TODO
+            }
+        }
+
+        // 最后的结果为gn和rn的组合
+        gn.addAll(rn);
+        return gn;
+    }
+
+    public List<Integer> grayCode(int n) {
+        init(n);
+        res.add(0);
+        dfs(0);
+        return new ArrayList<>(res);
+    }
+
+    private void dfs(int curCode) {
+        if (res.size() == grayCodeNum) {
             return;
         }
-
-        for (int i = 0; i <= 1; i++) {
-            list.add(i);
-            backtrack(level + 1, n, list);
-            list.remove(list.size() - 1);
-        }
-    }
-
-    /**
-     * 是否只有一bit不同
-     */
-    private boolean onlyOneBitDifferent(List<Integer> preGrayCode, List<Integer> list) {
-        int diffCount = 0;
-        for (int i = 0; i < preGrayCode.size(); i++) {
-            if (!preGrayCode.get(i).equals(list.get(i))) {
-                diffCount++;
-                if (diffCount > 1) {
-                    return false;
-                }
+        for (int mask : masks) {
+            int nextCode = curCode ^ mask;
+            if (!res.contains(nextCode)) {
+                res.add(nextCode);
+                dfs(nextCode);
             }
         }
-        return true;
     }
+
+    private void init(int n) {
+        masks = new int[n];
+        for (int i = 0; i < n; i++) {
+            masks[i] = 1 << i;
+        }
+        grayCodeNum = 1 << n;
+    }
+
 }
